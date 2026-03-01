ba966a05da593d446bb23f6ab7f087aaa2c54bea
unknown <dyahzhafira.w@gmail.com>
2026-03-01 15:21:22 +0700

    feat: add data and image for BoE BoM and asso

---
+++ data/asso.ts
@@ -0,0 +1,353 @@
+export const associates = [
+  // Data & Technology – Technology Development
+  {
+    id: "td-dnt-nezzaluna-azzahra",
+    name: "Nezzaluna Azzahra",
+    division: "Data & Technology",
+    team: "Technology Development"
+  },
+  {
+    id: "td-dnt-lyonel-judson",
+    name: "Lyonel Judson",
+    division: "Data & Technology",
+    team: "Technology Development"
+  },
+  {
+    id: "td-dnt-stefan-luciano",
+    name: "Stefan Luciano",
+    division: "Data & Technology",
+    team: "Technology Development"
+  },
+  {
+    id: "td-dnt-dyah-zhafira-wibowo",
+    name: "Dyah Zhafira Wibowo",
+    division: "Data & Technology",
+    team: "Technology Development"
+  },
+  {
+    id: "td-dnt-timothy-immanuel",
+    name: "Timothy Immanuel",
+    division: "Data & Technology",
+    team: "Technology Development"
+  },
+  {
+    id: "td-dnt-kenny-tang",
+    name: "Kenny Tang",
+    division: "Data & Technology",
+    team: "Technology Development"
+  },
+
+  // Data & Technology – Analytics and Research
+  {
+    id: "ar-dnt-ashley-meliora",
+    name: "Ashley Meliora",
+    division: "Data & Technology",
+    team: "Analytics and Research"
+  },
+  {
+    id: "ar-dnt-khalisha-zhurifah-sejati",
+    name: "Khalisha Zhurifah Sejati",
+    division: "Data & Technology",
+    team: "Analytics and Research"
+  },
+  {
+    id: "ar-dnt-nadine-keysha-joly-nugroho",
+    name: "Nadine Keysha Joly Nugroho",
+    division: "Data & Technology",
+    team: "Analytics and Research"
+  },
+  {
+    id: "ar-dnt-fauzan-rizky-ramadhan",
+    name: "Fauzan Rizky Ramadhan",
+    division: "Data & Technology",
+    team: "Analytics and Research"
+  },
+  {
+    id: "ar-dnt-sabrina-purwasantiana",
+    name: "Sabrina Purwasantiana",
+    division: "Data & Technology",
+    team: "Analytics and Research"
+  },
+  {
+    id: "ar-dnt-valsyahira-norayansha",
+    name: "Valsyahira Norayansha",
+    division: "Data & Technology",
+    team: "Analytics and Research"
+  },
+
+  // Human Resources – Talent Management
+  {
+    id: "tm-hr-hanif-muflih",
+    name: "Hanif Muflih",
+    division: "Human Resources",
+    team: "Talent Management"
+  },
+  {
+    id: "tm-hr-muh-fathihah-fachry-amal",
+    name: "Muh. Fathihah Fachry Amal",
+    division: "Human Resources",
+    team: "Talent Management"
+  },
+  {
+    id: "tm-hr-allea-nurra-kirania",
+    name: "Allea Nurra Kirania",
+    division: "Human Resources",
+    team: "Talent Management"
+  },
+  {
+    id: "tm-hr-nahran-aziza-aulia",
+    name: "Nahran Aziza Aulia",
+    division: "Human Resources",
+    team: "Talent Management"
+  },
+  {
+    id: "tm-hr-thara-nadda-ariezta",
+    name: "Thara Nadda Ariezta",
+    division: "Human Resources",
+    team: "Talent Management"
+  },
+
+  // Human Resources – Learning and Development
+  {
+    id: "lnd-hr-siti-rodiatul-ilaiah",
+    name: "Siti Rodiatul Ilaiah",
+    division: "Human Resources",
+    team: "Learning and Development"
+  },
+  {
+    id: "lnd-hr-darmanto",
+    name: "Darmanto",
+    division: "Human Resources",
+    team: "Learning and Development"
+  },
+  {
+    id: "lnd-hr-naurah-arista-putri",
+    name: "Naurah Arista Putri",
+    division: "Human Resources",
+    team: "Learning and Development"
+  },
+
+  // Human Resources – Employer Branding
+  {
+    id: "eb-hr-ammara-dyra-nafisa",
+    name: "Ammara Dyra Nafisa – Ammara",
+    division: "Human Resources",
+    team: "Employer Branding"
+  },
+  {
+    id: "eb-hr-paramadita-adnan",
+    name: "Paramadita Adnan",
+    division: "Human Resources",
+    team: "Employer Branding"
+  },
+  {
+    id: "eb-hr-mardiana-suhrul-romdoni",
+    name: "Mardiana Suhrul Romdoni – Dian",
+    division: "Human Resources",
+    team: "Employer Branding"
+  },
+
+  // Finance – Corporate Partnership
+  {
+    id: "cp-fin-i-putu-adi-ardana-yasa",
+    name: "I Putu Adi Ardana Yasa",
+    division: "Finance",
+    team: "Corporate Partnership"
+  },
+  {
+    id: "cp-fin-salsabila-siahaan",
+    name: "Salsabila Siahaan",
+    division: "Finance",
+    team: "Corporate Partnership"
+  },
+  {
+    id: "cp-fin-jessica-clarice-wong",
+    name: "Jessica Clarice Wong",
+    division: "Finance",
+    team: "Corporate Partnership"
+  },
+  {
+    id: "cp-fin-josephine-christy-then",
+    name: "Josephine Christy Then",
+    division: "Finance",
+    team: "Corporate Partnership"
+  },
+
+  // Finance – Business Unit
+  {
+    id: "bu-fin-marcel-antoni",
+    name: "Marcel Antoni",
+    division: "Finance",
+    team: "Business Unit"
+  },
+  {
+    id: "bu-fin-izabel-fathia-rafindria-saanin",
+    name: "Izabel Fathia Rafindria Saanin",
+    division: "Finance",
+    team: "Business Unit"
+  },
+  {
+    id: "bu-fin-alya-noor-fathiah",
+    name: "Alya Noor Fathiah",
+    division: "Finance",
+    team: "Business Unit"
+  },
+  {
+    id: "bu-fin-bagas-mardiansyah-sundawa",
+    name: "Bagas Mardiansyah Sundawa",
+    division: "Finance",
+    team: "Business Unit"
+  },
+
+  // Marketing – Creative
+  {
+    id: "cr-mkt-rizqi-suryo",
+    name: "Rizqi Suryo",
+    division: "Marketing",
+    team: "Creative"
+  },
+  {
+    id: "cr-mkt-ananda-rahmania",
+    name: "Ananda Rahmania",
+    division: "Marketing",
+    team: "Creative"
+  },
+  {
+    id: "cr-mkt-chesa-arwendhita-alifia",
+    name: "Chesa Arwendhita Alifia",
+    division: "Marketing",
+    team: "Creative"
+  },
+  {
+    id: "cr-mkt-sakurako-caya-katya-kato",
+    name: "Sakurako Caya Katya Kato",
+    division: "Marketing",
+    team: "Creative"
+  },
+  {
+    id: "cr-mkt-mutya-syafitri",
+    name: "Mutya Syafitri",
+    division: "Marketing",
+    team: "Creative"
+  },
+  {
+    id: "cr-mkt-tesalonika-putri-christvia",
+    name: "Tesalonika Putri Christvia",
+    division: "Marketing",
+    team: "Creative"
+  },
+
+  // Marketing – Social Media Specialist
+  {
+    id: "sms-mkt-keysha-khairunnisa",
+    name: "Keysha Khairunnisa",
+    division: "Marketing",
+    team: "Social Media Specialist"
+  },
+  {
+    id: "sms-mkt-luthfi-aqil-saputra",
+    name: "Luthfi Aqil Saputra",
+    division: "Marketing",
+    team: "Social Media Specialist"
+  },
+  {
+    id: "sms-mkt-zahra-khairunnisa",
+    name: "Zahra Khairunnisa",
+    division: "Marketing",
+    team: "Social Media Specialist"
+  },
+  {
+    id: "sms-mkt-evan-rizfi-novanda",
+    name: "Evan Rizfi Novanda",
+    division: "Marketing",
+    team: "Social Media Specialist"
+  },
+
+  // Marketing – External Affairs
+  {
+    id: "ea-mkt-adinda-naura-adhyaksari",
+    name: "Adinda Naura Adhyaksari",
+    division: "Marketing",
+    team: "External Affairs"
+  },
+  {
+    id: "ea-mkt-azemi-efan",
+    name: "Azemi Efan",
+    division: "Marketing",
+    team: "External Affairs"
+  },
+  {
+    id: "ea-mkt-ilyasa",
+    name: "Ilyasa",
+    division: "Marketing",
+    team: "External Affairs"
+  },
+
+  // Operations
+  {
+    id: "ops-bulan-khayangan",
+    name: "Bulan Khayangan",
+    division: "Operations",
+    team: "Operations"
+  },
+  {
+    id: "ops-elisabeth-michelle",
+    name: "Elisabeth Michelle",
+    division: "Operations",
+    team: "Operations"
+  },
+  {
+    id: "ops-mas-moreno-ivander-athala",
+    name: "Mas Moreno Ivander Athala",
+    division: "Operations",
+    team: "Operations"
+  },
+  {
+    id: "ops-novifrilita-karunia-suhardani",
+    name: "Novifrilita Karunia Suhardani",
+    division: "Operations",
+    team: "Operations"
+  },
+  {
+    id: "ops-mohammad-sang-panglima-anas",
+    name: "Mohammad Sang Panglima Anas",
+    division: "Operations",
+    team: "Operations"
+  },
+  {
+    id: "ops-naurazka-salsabila-aragani",
+    name: "Naurazka Salsabila Aragani",
+    division: "Operations",
+    team: "Operations"
+  },
+  {
+    id: "ops-kalya-salsabila-arizya",
+    name: "Kalya Salsabila Arizya",
+    division: "Operations",
+    team: "Operations"
+  },
+  {
+    id: "ops-jesslein-felisha",
+    name: "Jesslein Felisha",
+    division: "Operations",
+    team: "Operations"
+  },
+  {
+    id: "ops-sri-rahayu-lestari",
+    name: "Sri Rahayu Lestari",
+    division: "Operations",
+    team: "Operations"
+  },
+  {
+    id: "ops-anisya-syahwa-fitri",
+    name: "Anisya Syahwa Fitri",
+    division: "Operations",
+    team: "Operations"
+  },
+  {
+    id: "ops-yesha-trixie",
+    name: "Yesha Trixie",
+    division: "Operations",
+    team: "Operations"
+  }
+];
---
+++ data/boards.ts
@@ -0,0 +1,160 @@
+export const boards = [
+  // Executive Core
+  {
+    id: "ceo-nicholas",
+    name: "Nicholas Audric Adonis Mathew",
+    position: "Chief Executive Officer",
+    division: "CGC",
+    image: "/images/boards/ceo-nicholas.jpg"
+  },
+  {
+    id: "general-secretary-alisya",
+    name: "Alisya Mutiara Arsyisi",
+    position: "General Secretary",
+    division: "CGC",
+    image: "/images/boards/general-secretary-alisya.jpg"
+  },
+  {
+    id: "controller-andhika",
+    name: "Andhika Pratama",
+    position: "Controller",
+    division: "Executive",
+    image: "/images/boards/controller-andhika.jpg"
+  },
+
+  // Operations
+  {
+    id: "coo-darrel",
+    name: "Muhammad Darrell Damareka",
+    position: "Chief Operation Officer",
+    division: "Operations",
+    image: "/images/boards/coo-darrel.jpg"
+  },
+  {
+    id: "ops-mgr-fatimah",
+    name: "Fatimah Janahtun Azzahra",
+    position: "Manager of Operation",
+    division: "Operations",
+    image: "/images/boards/ops-mgr-fatimah.jpg"
+  },
+  {
+    id: "ops-mgr-dhia",
+    name: "Dhia Najma Shafa Jinan",
+    position: "Manager of Operation",
+    division: "Operations",
+    image: "/images/boards/ops-mgr-dhia.jpg"
+  },
+  {
+    id: "ops-mgr-rendy",
+    name: "Rendy Putra Bastanta Sitepu",
+    position: "Manager of Operation",
+    division: "Operations",
+    image: "/images/boards/ops-mgr-rendy.jpg"
+  },
+
+  // Marketing
+  {
+    id: "cmo-rasya",
+    name: "Rasya Amalya Putri",
+    position: "Chief Marketing Officer",
+    division: "Marketing",
+    image: "/images/boards/cmo-rasya.jpg"
+  },
+  {
+    id: "mkt-mgr-sms-aqila",
+    name: "Aqila Syakirah",
+    position: "Manager of Social Media Strategy",
+    division: "Marketing",
+    image: "/images/boards/mkt-mgr-sms-aqila.jpg"
+  },
+  {
+    id: "mkt-mgr-ea-refaya",
+    name: "Refaya Luvi Azizy",
+    position: "Manager of External Affairs",
+    division: "Marketing",
+    image: "/images/boards/mkt-mgr-ea-refaya.jpg"
+  },
+  {
+    id: "mkt-mgr-cvd-salma",
+    name: "Salma Haaniyah El Aliyyah",
+    position: "Manager of Creative & Visual Design",
+    division: "Marketing",
+    image: "/images/boards/mkt-mgr-cvd-salma.jpg"
+  },
+
+  // Human Resources
+  {
+    id: "chro-rona",
+    name: "Rona Sasia Nabila Harahap",
+    position: "Chief Human Resources Officer",
+    division: "Human Resources",
+    image: "/images/boards/chro-rona.jpg"
+  },
+  {
+    id: "hr-mgr-tm-alexandra",
+    name: "Alexandra Victoria Gunawan",
+    position: "Manager of Talent Management",
+    division: "Human Resources",
+    image: "/images/boards/hr-mgr-tm-alexandra.jpg"
+  },
+  {
+    id: "hr-mgr-lnd-daffa",
+    name: "Mochammad Daffa Rallibi",
+    position: "Manager of Learning & Development",
+    division: "Human Resources",
+    image: "/images/boards/hr-mgr-lnd-daffa.jpg"
+  },
+  {
+    id: "hr-mgr-eb-nursyafina",
+    name: "Nursyafina Apriliani Syukri",
+    position: "Manager of Employer Branding",
+    division: "Human Resources",
+    image: "/images/boards/hr-mgr-eb-nursyafina.jpg"
+  },
+
+  // Finance
+  {
+    id: "cfo-fazril",
+    name: "Fazril Harun",
+    position: "Chief Finance Officer",
+    division: "Finance",
+    image: "/images/boards/cfo-fazril.jpg"
+  },
+  {
+    id: "fin-mgr-cp-dzulfiqar",
+    name: "Muhammad Dzulfiqar Dhiaulhaq",
+    position: "Manager of Corporate Partnership",
+    division: "Finance",
+    image: "/images/boards/fin-mgr-cp-dzulfiqar.jpg"
+  },
+  {
+    id: "fin-mgr-bu-trisha",
+    name: "Trisha Nadhirah",
+    position: "Manager of Business & Finance",
+    division: "Finance",
+    image: "/images/boards/fin-mgr-bu-trisha.jpg"
+  },
+
+  // Data & Technology
+  {
+    id: "dnt-cdto-ari",
+    name: "Ari Muhamad Juliansyah",
+    position: "Manager of Data & Technology Development",
+    division: "Data & Technology",
+    image: "/images/boards/dnt-cdto-ari.jpg"
+  },
+  {
+    id: "dnt-mgr-td-richard",
+    name: "Richard Hans",
+    position: "Manager of Technology Development",
+    division: "Data & Technology",
+    image: "/images/boards/dnt-mgr-td-richard.jpg"
+  },
+  {
+    id: "dnt-mgr-anr-hasna",
+    name: "Hasna Choirunnisa",
+    position: "Manager of Analytics & Research",
+    division: "Data & Technology",
+    image: "/images/boards/dnt-mgr-anr-hasna.jpg"
+  }
+];

a7dd2e2ce043f5089cf8c8e80b69284629b2a255
unknown <dyahzhafira.w@gmail.com>
2026-03-03 16:37:22 +0700

    revise: delete and update research deck

--- data/mr-sxcamp.ts
+++
@@ -1,7 +0,0 @@
-export const mrSxCampSlides = Array.from({ length: 17 }, (_, i) => {
-  const pageNumber = String(i + 1).padStart(4, "0");
-  return {
-    image: `/mr-sxcamp/mr-sxcamp_page-${pageNumber}.jpg`,
-    alt: `MR SxCamp Slide ${i + 1}`,
-  };
-});
--- data/mr-sxcelerate.ts
+++ data/mr-sxcelerate.ts
@@ -1,7 +1,4 @@
-export const mrSxcelerateSlides = Array.from({ length: 17 }, (_, i) => {
-  const pageNumber = String(i + 1).padStart(4, "0");
-  return {
-    image: `/mr-sxcelerate/mr-sxcelerate_page-${pageNumber}.jpg`,
-    alt: `MR Sxcelerate Slide ${i + 1}`,
-  };
-});
+export const mrB13ResearchImages = Array.from({ length: 12 }, (_, i) => {
+  const pageNumber = String(i + 1).padStart(4, "0")
+  return `/mr-b13-research/sxc-b13-research_page-${pageNumber}.jpg`
+})
